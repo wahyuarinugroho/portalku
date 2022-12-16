a718d04cc309ab6844ef5aadd9ac023c5d52f78d
wahyuarinugroho <wahyuari.solo@gmail.com>
2022-12-16 16:28:34 +0700

    added first project

---
+++ dashboard/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from . models import Notes
+
+# Create your views here.
+
+def home(request):
+    return render(request,'dashboard/home.html')
+
+def notes(request):
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes':notes}
+    return render(request,'dashboard/notes.html',context)
