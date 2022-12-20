7832d3e09a41873b05cc833db5e216d15d1e2c89
wahyuarinugroho <wahyuari.solo@gmail.com>
2022-12-20 16:55:52 +0700

    Pembuatan fitur Notes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from . models import Notes
+from django.shortcuts import render, redirect
+from . forms import *
+from django.contrib import messages
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -7,6 +10,25 @@ def home(request):
     return render(request,'dashboard/home.html')
 
 def notes(request):
+    if request.method == "POST":
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+            messages.success(request,f"Notes Added from {request.user.username} Successfully!")
+            return redirect ("notes")
+        else:
+            messages.error(request, "terjadi kesalahan")
+            return redirect('notes')
+    else:
+        form = NotesForm()
     notes = Notes.objects.filter(user=request.user)
-    context = {'notes':notes}
+    context = {'notes':notes,'form':form}
     return render(request,'dashboard/notes.html',context)
+
+def delete_note(request,pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect("notes")
+
+class NotesDetailView(generic.DetailView):
+    model = Notes
