bbba1a48ae2783a95a139bebd921bcb7c1d9dd04
wahyuarinugroho <wahyuari.solo@gmail.com>
2022-12-23 17:09:04 +0700

    pembuatan fitur buku

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('todo',views.todo,name="todo"),
     path('update_todo/<int:pk>',views.update_todo,name="update-todo"),
     path('delete_todo/<int:pk>',views.delete_todo,name="delete-todo"),
-
+    path('book',views.book,name="book"),
 ]
